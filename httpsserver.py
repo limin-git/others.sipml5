748d8b7883c645aec6e74107da19764aa9203137
limin-git <zhulimin.china@gmail.com>
2023-07-21 01:02:05 +0800

    home

--- httpsserver.py
+++ httpsserver.py
@@ -1,12 +1,12 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
 
-httpd = HTTPServer(('192.168.10.198', 4443), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('', 4443), SimpleHTTPRequestHandler)
 
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
-                               keyfile=r"C:\Users\zhuli\key.pem",
-                               certfile=r'C:\Users\zhuli\cert.pem',
+                               keyfile=r"D:\Temp\key.pem",
+                               certfile=r'D:\Temp\cert.pem',
                                ssl_version=ssl.PROTOCOL_TLS)
 
 httpd.serve_forever()
